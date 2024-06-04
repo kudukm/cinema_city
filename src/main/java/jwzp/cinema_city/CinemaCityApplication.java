a439c82466e1714cc8af927f553be402f8a71832
kudukm <kuduk.marcin@gmail.com>
2024-06-04 11:03:38 +0200

    Basic Spring app created

---
+++ src/main/java/jwzp/cinema_city/CinemaCityApplication.java
@@ -0,0 +1,13 @@
+package jwzp.cinema_city;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaCityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jwzp/cinema_city/CinemaCityApplicationTests.java
@@ -0,0 +1,13 @@
+package jwzp.cinema_city;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
