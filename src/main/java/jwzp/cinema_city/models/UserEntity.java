2e2fbcae9dcde46d1ad66e066e9cab371f98c698
Renis-1 <jabystrowski@gmail.com>
2024-06-13 18:47:22 +0200

    made username unique

--- src/main/java/jwzp/cinema_city/models/UserEntity.java
+++ src/main/java/jwzp/cinema_city/models/UserEntity.java
@@ -1,12 +1,14 @@
 package jwzp.cinema_city.models;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
 public class UserEntity {
     @Id
     private String id;
+    @Indexed(unique = true)
     private String username;
     private String password;
     private String email;
