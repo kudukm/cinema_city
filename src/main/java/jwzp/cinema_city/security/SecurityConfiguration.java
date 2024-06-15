7af415ed416d2267b181bb947a11c1d6d2dcde4d
Renis-1 <jabystrowski@gmail.com>
2024-06-15 12:33:40 +0200

    adden missing authenticationManager bean

--- src/main/java/jwzp/cinema_city/security/SecurityConfiguration.java
+++ src/main/java/jwzp/cinema_city/security/SecurityConfiguration.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
@@ -48,6 +50,11 @@ public class SecurityConfiguration {
     @Bean
     public UserService userDetailsService() {
         return userService;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
