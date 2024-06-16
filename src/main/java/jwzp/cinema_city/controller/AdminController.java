fe975079a684daf255f346abb24f34b4004e2b9f
Renis-1 <jabystrowski@gmail.com>
2024-06-16 21:23:32 +0200

    creaded a nice form to addScreening

--- src/main/java/jwzp/cinema_city/controller/AdminController.java
+++ src/main/java/jwzp/cinema_city/controller/AdminController.java
@@ -40,16 +40,19 @@ public class AdminController {
     }
 
     @GetMapping("/api/admin/addScreening")
-    public ResponseEntity<Pair<List<Movie>,Screening>> showAddScreeningForm() {
-        List<Movie> movies = movieService.findAllMovies();
-        Pair<List<Movie>,Screening> response = Pair.of(movies, new Screening());
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public ResponseEntity<List<Movie>> showAddScreeningForm() {
+        List<Movie> movies = movieService.findAllMovies();;
+        return new ResponseEntity<>(movies, HttpStatus.OK);
     }
 
     @PostMapping("/admin/addScreening")
     public ResponseEntity<String> addScreening(@RequestBody Screening screening) {
-        screeningService.addScreening(screening);
-        return new  ResponseEntity<>("Created new Screening successfully", HttpStatus.CREATED);
+        try {
+            screeningService.addScreening(screening);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Screening added successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to add the screening. Please try again.");
+        }
     }
 
 }
