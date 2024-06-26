b714f0d95e28fd25b2668d17280d7bc2386a3a05
Renis-1 <jabystrowski@gmail.com>
2024-06-26 23:06:15 +0200

    changed dependency injection so that it will work with mocks adn added AuthServiceTest

--- src/main/java/jwzp/cinema_city/service/AuthService.java
+++ src/main/java/jwzp/cinema_city/service/AuthService.java
@@ -3,6 +3,7 @@ package jwzp.cinema_city.service;
 import jwzp.cinema_city.models.AuthRequest;
 import jwzp.cinema_city.models.UserEntity;
 import jwzp.cinema_city.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,21 +11,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
-    private final UserRepository userRepository;
-
-    private final PasswordEncoder passwordEncoder;
-
-    private final AuthenticationManager authenticationManager;
-
-    public AuthService(
-            UserRepository userRepository,
-            AuthenticationManager authenticationManager,
-            PasswordEncoder passwordEncoder
-    ) {
-        this.authenticationManager = authenticationManager;
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
+    @Autowired
+    private  UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     public UserEntity registerUser(UserEntity input) {
         UserEntity user = new UserEntity();
